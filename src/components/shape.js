f5e2b520b6d2a470355aaa8228f4b094143fe520
inapanuri <60398472+inapanuri@users.noreply.github.com>
2020-10-31 20:03:39 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,17 @@
 // declare class
 
 // export class using module.exports
+module.exports=class Shape
+{
+    constructor(){
+        this.color="red";
+    }
+  drawShape()
+  {
+    console.log("Drawing");
+  }
+  calculateArea()
+  {
+    console.log("calculating");
+  }
+}
